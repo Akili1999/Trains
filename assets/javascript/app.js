308ea1da825434df5968a8a6e97dfe5b4473c608
unknown <sluground@gmail.com>
2019-04-27 12:18:13 -0400

    moment added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,14 @@ var config = {
   firebase.initializeApp(config);
 
 
-// var name = "";
-// var destination = "";
-// var time = 0;
+var name = "";
+var destination = "";
+// var time = "";
 // var frequency = 0;
+// var converted = moment(time, "HH:mm").subtract(1, "years");
+// var current = moment();
+// var diff = moment().diff(moment(converted), "minutes");
+// var remainder = diff % frequency;
 // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 // var currentTime = moment();
 // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
@@ -26,29 +30,60 @@ $("#add-train").on("click", function(){
   name = $("#name-input").val().trim();
   destination = $("#destination-input").val().trim();
   time = $("#time-input").val().trim();
-  frequency = $("#frequency-input").val().trim();
-  firebase.database().ref().push({
-    name:name,
-    destination:destination,
-    time:time,
-    frequency:frequency,
-    dataAdded:firebase.database.ServerValue.TIMESTAMP
-  })
+  console.log(time.length);
+  console.log(time[2]);
+  console.log(validate(time));
+  if (time.length !== 5 || time[2] !== ':' || validate(time) !== true) {
+    console.log('gonna break here..');
+    
+  } else {
+    frequency = $("#frequency-input").val().trim();
+    firebase.database().ref().push({
+      name:name,
+      destination:destination,
+      time:time,
+      frequency:frequency,
+      dataAdded:firebase.database.ServerValue.TIMESTAMP
+    })
+  }
   }
 })
-firebase.database().ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-  $("#name-display").html(snapshot.val().name);
-  $("#destination-display").html(snapshot.val().destination);
-  $("#time-display").html(snapshot.val().time);
-  $("#frequency-display").html(snapshot.val().frequency);
-})
+
+function validate (time) {
+  var isGood = true;
+  for (let i = 0; i < time.length; i++ ){
+    if(time[i] !== ':') {
+      console.log(typeof(time[i]));
+      if (Number.isInteger(parseInt(time[i])) == false)  {
+        console.log('not a valid time');
+        isGood = false;
+        break;
+      } 
+    }
+  }
+  return isGood;
+}
 
 firebase.database().ref().on("child_added", function(snapshot){
+  var time = "";
+  var frequency = 0;
+  var converted = moment(snapshot.val().time, "HH:mm").subtract(1, "years");
+  var current = moment();
+  var diff = moment().diff(moment(converted), "minutes");  
+  console.log(diff);
+  console.log(typeof(snapshot.val().frequency));
+  
+  
+  remainder = diff % parseInt(snapshot.val().frequency);
+  console.log(remainder);
+  
+  var tillTrain = snapshot.val().frequency - remainder;
   var row = $("<tr>")
   row.append("<td>"+snapshot.val().name+"</td>");
   row.append("<td>"+snapshot.val().destination+"</td>");
   row.append("<td>"+snapshot.val().frequency+"</td>");
   row.append("<td>"+snapshot.val().time+"</td>");
+  row.append("<td>"+ tillTrain + "</td>");
   $(".fire-table").append(row)
-
+  
 })
